17bbc50e2083f84a5d431af7d78629e8c83e81ca
Stephen Haberman <stephen.haberman@gmail.com>
2025-03-29 13:34:09 -0500

    Fix config.

--- packages/benchmark-mikroorm/src/mikro-orm.config.ts
+++ packages/benchmark-mikroorm/src/mikro-orm.config.ts
@@ -1,25 +1,17 @@
-import { NamingStrategy, Options, UnderscoreNamingStrategy } from '@mikro-orm/core';
-import { PostgreSqlDriver } from '@mikro-orm/postgresql';
-import { Author, Book, BookReview, Tag } from './entities';
+import { defineConfig, PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { DB_CONFIG } from 'shared-utils';
+import { Author, Book, BookReview, Tag } from './entities';
 
-// Create a custom naming strategy that preserves camelCase
-class CamelCaseNamingStrategy extends UnderscoreNamingStrategy {
-  propertyToColumnName(propertyName: string): string {
-    return propertyName;
-  }
-}
 
-const config: Options<PostgreSqlDriver> = {
+const config = defineConfig({
   entities: [Author, Book, BookReview, Tag],
   dbName: DB_CONFIG.database,
   host: DB_CONFIG.host,
   port: DB_CONFIG.port,
   user: DB_CONFIG.username,
   password: DB_CONFIG.password,
-  type: 'postgresql',
+  driver: PostgreSqlDriver,
   debug: false,
-  namingStrategy: CamelCaseNamingStrategy,
-};
+});
 
 export default config;

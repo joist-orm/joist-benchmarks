4d73813eb9f6383a2ca61dd61812a0258cb4779d
Stephen Haberman <stephen.haberman@gmail.com>
2026-07-22 20:03:42 -0500

    Fix joist v2 moving to native pg.

--- packages/benchmark-joist-v2/src/index.ts
+++ packages/benchmark-joist-v2/src/index.ts
@@ -1,25 +1,27 @@
 import { PostgresDriver } from "joist-orm/pg";
+import pg from "pg";
 import { AllOperations, Context, getDatabaseUrl, Operation } from "seed-data";
 import { bulkCreate } from "./bulk-create.ts";
 import { bulkLoad } from "./bulk-load.ts";
 import { loadInLoop } from "./load-in-loop.ts";
 import { findInLoop } from "./find-in-loop.ts";
 import { simpleCreate } from "./simple-create.ts";
-import knex, { type Knex } from "knex";
 
-export type JoistContext = Context & { conn: Knex; driver: PostgresDriver; preload: boolean };
+const { Pool } = pg;
+
+export type JoistContext = Context & { pool: pg.Pool; driver: PostgresDriver; preload: boolean };
 export type JoistOperation = Operation<JoistContext>;
 
-export async function getContext(): Promise<Pick<JoistContext, "driver" | "shutdown" | "conn" | "preload">> {
-  const conn = knex({ client: "pg", connection: getDatabaseUrl("joist_v2") });
-  const driver = new PostgresDriver(conn);
-  return { conn, driver, shutdown: () => conn.destroy(), preload: false };
+export async function getContext(): Promise<Pick<JoistContext, "driver" | "shutdown" | "pool" | "preload">> {
+  const pool = new Pool({ connectionString: getDatabaseUrl("joist_v2") });
+  const driver = new PostgresDriver(pool);
+  return { pool, driver, shutdown: () => pool.end(), preload: false };
 }
 
-export async function getContextPreload(): Promise<Pick<JoistContext, "driver" | "shutdown" | "conn" | "preload">> {
-  const conn = knex({ client: "pg", connection: getDatabaseUrl("joist_v2") });
-  const driver = new PostgresDriver(conn);
-  return { conn, driver, shutdown: () => conn.destroy(), preload: true };
+export async function getContextPreload(): Promise<Pick<JoistContext, "driver" | "shutdown" | "pool" | "preload">> {
+  const pool = new Pool({ connectionString: getDatabaseUrl("joist_v2") });
+  const driver = new PostgresDriver(pool);
+  return { pool, driver, shutdown: () => pool.end(), preload: true };
 }
 
 export function getOperations(): AllOperations<JoistContext> {
@@ -27,5 +29,5 @@ export function getOperations(): AllOperations<JoistContext> {
 }
 
 export async function cleanDatabase(ctx: JoistContext): Promise<void> {
-  await ctx.conn.raw(`TRUNCATE book_tag, book_review, book, author, tag RESTART IDENTITY CASCADE`);
+  await ctx.pool.query(`TRUNCATE book_tag, book_review, book, author, tag RESTART IDENTITY CASCADE`);
 }
